a63f732ed8f2f759a4107cc6e9e3b38aa26a21a3
Dimaspadma <dimaspadma@gmail.com>
2023-02-09 00:28:53 +0700

    Refactoring code to add namespace

--- Assets/Scripts/PlayerRobot.cs
+++ Assets/Scripts/PlayerRobot.cs
@@ -1,12 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerRobot : MonoBehaviour
+namespace Playground
 {
-    public string robotName;
-    public int robotLevel;
-    public int damage;
-    public int maxHP;
-    public int currentHP;
+    public class PlayerRobot : MonoBehaviour
+    {
+        public string robotName;
+        public int robotLevel;
+        public int damage;
+        public int maxHP;
+        public int currentHP;
+    }
 }
