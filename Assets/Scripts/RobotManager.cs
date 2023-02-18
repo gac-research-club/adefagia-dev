19fd2d6ab45fc592cb8ab45470e90915b9336332
Dimaspadma <dimaspadma@gmail.com>
2023-02-18 17:30:55 +0700

    Add Game, Robot & UI Manager

---
+++ Assets/Scripts/RobotManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using adefagia.Robot;
+using UnityEngine;
+
+namespace adefagia
+{
+    public class RobotManager : MonoBehaviour
+    {
+        public Spawner spawner;
+    }
+}
