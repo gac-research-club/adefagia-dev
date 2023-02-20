dfb855e61dd211b8e1b06a4013abfeffab48755c
DzawilUqul <112853502+DzawilUqul@users.noreply.github.com>
2023-02-20 23:37:13 +0700

    Refactoring input system using EventHandler delegate

--- Assets/Scripts/OutlineSelection.cs
+++ Assets/Scripts/OutlineSelection.cs
@@ -9,7 +9,39 @@ namespace adefagia
         private Transform selection;
         private RaycastHit raycastHit;
 
+        [SerializeField] private GameInput gameInput;
+        [SerializeField] private GameObject actionButton;
         [SerializeField] private Color color;
+
+        private void Start()
+        {
+            gameInput.OnInteractAction += GameInput_OnInteractAction;
+        }
+
+        private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+        {
+            // Selection
+            if (highlight)
+            {
+                if (selection != null)
+                {
+                    selection.gameObject.GetComponent<Outline>().enabled = false;
+                }
+                selection = raycastHit.transform;
+                selection.gameObject.GetComponent<Outline>().enabled = true;
+                highlight = null;
+                actionButton.SetActive(true);
+            }
+            else
+            {
+                if (selection)
+                {
+                    selection.gameObject.GetComponent<Outline>().enabled = false;
+                    selection = null;
+                    actionButton.SetActive(false);
+                }
+            }
+        }
 
         void Update()
         {
@@ -40,29 +72,6 @@ namespace adefagia
                 else
                 {
                     highlight = null;
-                }
-            }
-
-            // Selection
-            if (Input.GetMouseButtonDown(0))
-            {
-                if (highlight)
-                {
-                    if (selection != null)
-                    {
-                        selection.gameObject.GetComponent<Outline>().enabled = false;
-                    }
-                    selection = raycastHit.transform;
-                    selection.gameObject.GetComponent<Outline>().enabled = true;
-                    highlight = null;
-                }
-                else
-                {
-                    if (selection)
-                    {
-                        selection.gameObject.GetComponent<Outline>().enabled = false;
-                        selection = null;
-                    }
                 }
             }
         }
