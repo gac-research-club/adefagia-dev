22a84a2df776ab4a24c186f6ff2daf12b6ec72ff
DzawilUqul <112853502+DzawilUqul@users.noreply.github.com>
2023-02-20 22:29:06 +0700

    Add Outline Highlight

---
+++ Assets/Scripts/OutlineSelection.cs
@@ -0,0 +1,70 @@
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+namespace adefagia
+{
+    public class OutlineSelection : MonoBehaviour
+    {
+        private Transform highlight;
+        private Transform selection;
+        private RaycastHit raycastHit;
+
+        [SerializeField] private Color color;
+
+        void Update()
+        {
+            // Highlight
+            if (highlight != null)
+            {
+                highlight.gameObject.GetComponent<Outline>().enabled = false;
+                highlight = null;
+            }
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit)) //Make sure you have EventSystem in the hierarchy before using EventSystem
+            {
+                highlight = raycastHit.transform;
+                if (highlight.CompareTag("Selectable") && highlight != selection)
+                {
+                    if (highlight.gameObject.GetComponent<Outline>() != null)
+                    {
+                        highlight.gameObject.GetComponent<Outline>().enabled = true;
+                    }
+                    else
+                    {
+                        Outline outline = highlight.gameObject.AddComponent<Outline>();
+                        outline.enabled = true;
+                        highlight.gameObject.GetComponent<Outline>().OutlineColor = color;
+                        highlight.gameObject.GetComponent<Outline>().OutlineWidth = 8.0f;
+                    }
+                }
+                else
+                {
+                    highlight = null;
+                }
+            }
+
+            // Selection
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (highlight)
+                {
+                    if (selection != null)
+                    {
+                        selection.gameObject.GetComponent<Outline>().enabled = false;
+                    }
+                    selection = raycastHit.transform;
+                    selection.gameObject.GetComponent<Outline>().enabled = true;
+                    highlight = null;
+                }
+                else
+                {
+                    if (selection)
+                    {
+                        selection.gameObject.GetComponent<Outline>().enabled = false;
+                        selection = null;
+                    }
+                }
+            }
+        }
+    }
+}
