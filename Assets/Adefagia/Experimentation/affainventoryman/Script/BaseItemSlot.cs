15e49bdbd15881f425fa14bd12e4fef5d79670ec
Alifian Adexe <alifianadexe@gmail.com>
2023-06-13 13:13:35 +0700

    75 feature inventory text (#85)
    * push new slot item
    
    * make new consumable item
    
    * Refactor naming variable & function
    
    * - Add Main menu include option menu
    - Add dummy item (Top, Body, Weapon)
    - Add Choose Mode menu
    
    * - Make Main menu looks new
    - Fix bug on audio
    - Make sure Option menu its work
    - Create Inventory or Armory scene looks better with implementations
    
    * - Create some new on Inventory Scene
    - Create new Dummy Item on Inventory
    
    ---------
    
    Co-authored-by: Akuanaksehatt <111202013075@mhs.dinus.id>
    Co-authored-by: Dimaspadma <Dimaspadma@users.noreply.github.com>

--- Assets/Adefagia/Experimentation/affainventoryman/Script/BaseItemSlot.cs
+++ Assets/Adefagia/Experimentation/affainventoryman/Script/BaseItemSlot.cs
@@ -5,70 +5,74 @@ using UnityEngine.EventSystems;
 
 public class BaseItemSlot : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
-	[SerializeField] protected Image image;
-	[SerializeField] protected Text amountText;
+    [SerializeField] protected Image image;
+    [SerializeField] protected Text amountText;
 
-	public event Action<BaseItemSlot> OnPointerEnterEvent;
-	public event Action<BaseItemSlot> OnPointerExitEvent;
-	public event Action<BaseItemSlot> OnRightClickEvent;
+    public event Action<BaseItemSlot> OnPointerEnterEvent;
+    public event Action<BaseItemSlot> OnPointerExitEvent;
+    public event Action<BaseItemSlot> OnRightClickEvent;
 
     protected bool isPointerOver;
 
-	protected Color normalColor = Color.white;
-	protected Color disabledColor = new Color(1, 1, 1, 0);
+    protected Color normalColor = Color.white;
+    protected Color disabledColor = new Color(1, 1, 1, 0);
 
-	protected Item _item;
-	public Item Item {
-		get { return _item; }
-		set {
-			_item = value;
+    protected Item _item;
+    public Item Item
+    {
+        get { return _item; }
+        set
+        {
+            _item = value;
             if (_item == null && Amount != 0) Amount = 0;
 
-			if (_item == null)
-			{
-				image.color = disabledColor;
-			}
-			else
-			{
-				image.sprite = _item.Icon;
-				image.color = normalColor;
-			}
+            if (_item == null)
+            {
+                image.color = disabledColor;
+            }
+            else
+            {
+                image.sprite = _item.Icon;
+                image.color = normalColor;
+            }
 
             if (isPointerOver)
             {
                 OnPointerEnter(null);
                 OnPointerExit(null);
             }
-		}
-	}
+        }
+    }
 
-	private int _amount;
-	public int Amount {
-		get { return _amount; }
-		set {
-			_amount = value;
+    private int _amount;
+    public int Amount
+    {
+        get { return _amount; }
+        set
+        {
+            _amount = value;
             if (_amount < 0) _amount = 0;
             if (_amount == 0 && Item != null) Item = null;
 
             if (amountText != null)
             {
                 amountText.enabled = _item != null && _item.MaximumStacks > 1 && _amount > 1;
-			    if (amountText.enabled) 
+                if (amountText.enabled)
                 {
-				    amountText.text = _amount.ToString();
+                    amountText.text = _amount.ToString();
                 }
-			}
-		}
-	}
+            }
+        }
+    }
 
-	protected virtual void OnValidate()
-	{
-		if (image == null)
-			image = GetComponent<Image>();
+    protected virtual void OnValidate()
+    {
+        if (image == null)
+            image = GetComponent<Image>();
 
-		if (amountText == null)
-			amountText = GetComponentInChildren<Text>();
-	}
+        if (amountText == null)
+            amountText = GetComponentInChildren<Text>();
+    }
 
     protected virtual void OnDisable()
     {
@@ -82,47 +86,49 @@ public class BaseItemSlot : MonoBehaviour, IPointerClickHandler, IPointerEnterHa
     {
         return Item != null && Item.ID == item.ID;
     }
-    
+
     public virtual bool RemoveStack(int amount = 1)
- 	{
-		if (Amount < amount) {
-			return false;
-		}
- 
-		Amount -= amount;
-		if (Amount == 0) {
-			Item = null;
-		}
-		return true;
-	}
+    {
+        if (Amount < amount)
+        {
+            return false;
+        }
 
-	public virtual bool CanReceiveItem(Item item)
-	{
-		return false;
-	}
+        Amount -= amount;
+        if (Amount == 0)
+        {
+            Item = null;
+        }
+        return true;
+    }
 
-	public void OnPointerClick(PointerEventData eventData)
-	{
-		if (eventData != null && eventData.button == PointerEventData.InputButton.Right)
-		{
-			if (OnRightClickEvent != null)
-				OnRightClickEvent(this);
-		}
-	}
+    public virtual bool CanReceiveItem(Item item)
+    {
+        return false;
+    }
 
-	public void OnPointerEnter(PointerEventData eventData)
-	{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData != null && eventData.button == PointerEventData.InputButton.Right)
+        {
+            if (OnRightClickEvent != null)
+                OnRightClickEvent(this);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
         isPointerOver = true;
 
-		if (OnPointerEnterEvent != null)
-			OnPointerEnterEvent(this);
-	}
+        if (OnPointerEnterEvent != null)
+            OnPointerEnterEvent(this);
+    }
 
-	public void OnPointerExit(PointerEventData eventData)
-	{
+    public void OnPointerExit(PointerEventData eventData)
+    {
         isPointerOver = false;
 
-		if (OnPointerExitEvent != null)
-			OnPointerExitEvent(this);
-	}
+        if (OnPointerExitEvent != null)
+            OnPointerExitEvent(this);
+    }
 }
