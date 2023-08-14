7b50102d5b10a3cff00800c547cc23b15ee07d31
Dimaspadma <dimaspadma@users.noreply.github.com>
2023-08-14 16:34:48 +0700

    Change Move camera system

--- Assets/ZoomCamera.cs
+++ Assets/ZoomCamera.cs
@@ -1,15 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class ZoomCamera : MonoBehaviour
 {
     [SerializeField] private float scrollScale;
-    
+    [SerializeField] private Vector3 zoomAmount;
     [SerializeField] private float maxZoom;
     [SerializeField] private float minZoom;
+    [SerializeField] private Vector3 maxZoomPerspective;
+    [SerializeField] private Vector3 minZoomPerspective;
+
+    private Vector3 newZoom;
 
     private Camera _camera;
 
@@ -20,33 +25,62 @@ public class ZoomCamera : MonoBehaviour
     private void Start()
     {
         _camera = GetComponent<Camera>();
+        newZoom = transform.localPosition;
+        
     }
 
     private void Update()
     {
         // Get Scroll mouse input
-        _camera.orthographicSize -= Input.mouseScrollDelta.y * scrollScale;
-        
-        if (_camera.orthographicSize < minZoom)
-        {
-            _camera.orthographicSize = minZoom;
-            _isLimitZoom = true;
-        }
+        HandleInput();
+        HandleZoomInput();
+    }
 
-        if (_camera.orthographicSize > maxZoom)
+    private void HandleInput()
+    {
+        if (_camera.orthographic)
         {
-            _camera.orthographicSize = maxZoom;
-            _isLimitZoom = true;
-        }
+            _camera.orthographicSize -= Input.mouseScrollDelta.y * scrollScale;
+        
+            if (_camera.orthographicSize < minZoom)
+            {
+                _camera.orthographicSize = minZoom;
+                _isLimitZoom = true;
+            }
 
-        if (_camera.orthographicSize > minZoom && _camera.orthographicSize < maxZoom)
+            if (_camera.orthographicSize > maxZoom)
+            {
+                _camera.orthographicSize = maxZoom;
+                _isLimitZoom = true;
+            }
+
+            if (_camera.orthographicSize > minZoom && _camera.orthographicSize < maxZoom)
+            {
+                _isLimitZoom = false;
+            }
+        }
+    }
+    
+    private void HandleZoomInput()
+    {
+        if(_camera.orthographic) return;
+        
+        if (Input.mouseScrollDelta.y != 0)
         {
-            _isLimitZoom = false;
+            newZoom += Input.mouseScrollDelta.y * zoomAmount;
         }
         
-        if (!_isLimitZoom)
-        {
-            Zooming?.Invoke(Input.mouseScrollDelta.y * scrollScale);
-        }
+        // MinMaxZoom(newZoom, minZoomPerspective, maxZoomPerspective);
+
+        transform.DOLocalMove(
+            newZoom, 
+            scrollScale);
+        
+    }
+
+    private void MinMaxZoom(Vector3 current, Vector3 min, Vector3 max)
+    {
+        var direction = current - max;
+        Debug.Log(direction.normalized);
     }
 }
