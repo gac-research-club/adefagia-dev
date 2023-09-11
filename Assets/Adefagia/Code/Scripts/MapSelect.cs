9369c86fe4563166b8de75a9683459c2f2b97a7f
Hanif <85260485+hxnmi@users.noreply.github.com>
2023-09-12 00:38:54 +0700

    Some Adjustment parameter for 1v1
    - Active 1v1 button in ChooseMode
    - Adjust Inventory for 1 Robot
    - Scene Management for 1v1

--- Assets/Adefagia/Code/Scripts/MapSelect.cs
+++ Assets/Adefagia/Code/Scripts/MapSelect.cs
@@ -2,47 +2,71 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using Adefagia;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MapSelect : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI textMeshProUGUI;
-    [SerializeField] private string scene;
-    [SerializeField] private bool canCreateNew;
+	[SerializeField] private TextMeshProUGUI textMeshProUGUI;
+	// [SerializeField] private string scene;
+	[SerializeField] private bool canCreateNew;
 
-    public void Choose()
-    {
-        var mapName = textMeshProUGUI.text;
-        mapName = mapName
-            .Replace("/r", "")
-            .Replace("\u200B", "")
-            .Replace("/n", "");
-        
-        var path = $"{Application.persistentDataPath}/Map/{mapName}.json";
+	private TeamManager _teamManager;
 
-        if (File.Exists(path) || canCreateNew)
-        {
-            PlayerPrefs.SetString("Map", mapName);
-            SceneManager.LoadScene(scene);
-        }
-        else
-        {
-            Debug.LogWarning("Map Not found");
-        }
-    }
+	private void Start()
+	{
+		_teamManager = GameManager.instance.teamManager;
+	}
 
-    public void Random()
-    {
-        PlayerPrefs.SetString("Map", "<random>");
-        SceneManager.LoadScene(scene);
-    }
+	public void Choose()
+	{
+		var mapName = textMeshProUGUI.text;
+		mapName = mapName
+			.Replace("/r", "")
+			.Replace("\u200B", "")
+			.Replace("/n", "");
 
-    private void OnGUI()
-    {
-        GUILayout.BeginArea(new Rect(10,10,200,200));
-        if (GUILayout.Button("Back")) SceneManager.LoadScene("MainMenuAffaUtama");
-        GUILayout.EndArea();
-    }
+		var path = $"{Application.persistentDataPath}/Map/{mapName}.json";
+
+		if (File.Exists(path) || canCreateNew)
+		{
+			if (_teamManager.totalRobot == 1)
+			{
+				PlayerPrefs.SetString("Map", mapName);
+				SceneManager.LoadScene("1v1");
+			}
+			else
+			{
+				PlayerPrefs.SetString("Map", mapName);
+				SceneManager.LoadScene("Battle");
+			}
+		}
+		else
+		{
+			Debug.LogWarning("Map Not found");
+		}
+	}
+
+	public void Random()
+	{
+		if (_teamManager.totalRobot == 1)
+		{
+			PlayerPrefs.SetString("Map", "<random>");
+			SceneManager.LoadScene("1v1");
+		}
+		else
+		{
+			PlayerPrefs.SetString("Map", "<random>");
+			SceneManager.LoadScene("Battle");
+		}
+	}
+
+	private void OnGUI()
+	{
+		GUILayout.BeginArea(new Rect(10, 10, 200, 200));
+		if (GUILayout.Button("Back")) SceneManager.LoadScene("MainMenuAffaUtama");
+		GUILayout.EndArea();
+	}
 }
--- Assets/Adefagia/Experimentation/affainventoryman/Script/StatPanel.cs
+++ Assets/Adefagia/Experimentation/affainventoryman/Script/StatPanel.cs
@@ -12,269 +12,298 @@ using UnityEngine.UI;
 
 public class StatPanel : MonoBehaviour
 {
-    [SerializeField] StatDisplay[] statDisplays;
-    [SerializeField] string[] statNames;
-
-    [SerializeField] Text characterName;
-
-    [SerializeField] private Button buttonBattle;
-
-    private CharacterStat[] stats;
-
-    private static int indexRobot = 0;
-
-    private TeamManager _teamManager;
-    private Team _currentTeam;
-
-    private ItemState _itemState = ItemState.Initialize;
-
-
-    private void Start()
-    {
-        // Initiate Robots
-        _teamManager = GameManager.instance.teamManager;
-
-        // Initial Name 
-        _currentTeam = _teamManager.teamA;
-        characterName.text = "Robot " + indexRobot.ToString() + " | Team " + _currentTeam.teamName;
-        
-        statDisplays = GetComponentsInChildren<StatDisplay>();
-        UpdateStatNames();
-    }
-
-    private void Update()
-    {
-        
-        if (_itemState == ItemState.Update)
-        {
-            // Update damage value
-            _teamManager.GetRobots(_currentTeam)[indexRobot].damage = statDisplays[0].Stat.Value;
-            
-            // Update armor value
-            _teamManager.GetRobots(_currentTeam)[indexRobot].armor = statDisplays[1].Stat.Value;
-
-        }
-
-    }
-
-    private void OnValidate()
-    {
-        Debug.Log("OnValidate");
-        _itemState = ItemState.Update;
-    }
-
-    // charStats = Armor, Attack
-    public void SetStats(params CharacterStat[] charStats)
-    {
-        stats = charStats;
-
-        // below stat display length
-        if (stats.Length > statDisplays.Length)
-        {
-            Debug.LogError("Not Enough Stat Display!");
-            return;
-        }
-
-        for (int i = 0; i < statDisplays.Length; i++)
-        {
-            statDisplays[i].gameObject.SetActive(i < stats.Length);
-
-            if (i < stats.Length)
-            {
-                statDisplays[i].Stat = stats[i];
-            }
-        }
-    }
-
-    public void UpdateStatValues()
-    {
-        for (int i = 0; i < stats.Length; i++)
-        {
-            statDisplays[i].UpdateStatValue();
-        }
-    }
-
-    public void UpdateStatNames()
-    {
-        _itemState = ItemState.Update;
-        for (int i = 0; i < statNames.Length; i++)
-        {
-            statDisplays[i].Name = statNames[i];
-        }
-    }
-    public void UpdateUsableItemId(UsableItem item, string type)
-    {
-        if (type == EquipmentType.BuffItem1.ToString())
-        {
-
-            // Update Buff Item 1 id
-            _teamManager.GetRobots(_currentTeam)[indexRobot].buffItem1 = item;
-
-        }
-        else if (type == EquipmentType.BuffItem2.ToString())
-        {
+	[SerializeField] StatDisplay[] statDisplays;
+	[SerializeField] string[] statNames;
+
+	[SerializeField] Text characterName;
+
+	[SerializeField] private Button buttonBattle;
+
+	private CharacterStat[] stats;
+
+	private static int indexRobot = 0;
+
+	private TeamManager _teamManager;
+	private Team _currentTeam;
+
+	private ItemState _itemState = ItemState.Initialize;
+
+
+	private void Start()
+	{
+		// Initiate Robots
+		_teamManager = GameManager.instance.teamManager;
+
+		// Initial Name 
+		_currentTeam = _teamManager.teamA;
+		characterName.text = "Robot " + indexRobot.ToString() + " | Team " + _currentTeam.teamName;
+
+		statDisplays = GetComponentsInChildren<StatDisplay>();
+		UpdateStatNames();
+	}
+
+	private void Update()
+	{
 
-            // Update Buff Item 2 id
-            _teamManager.GetRobots(_currentTeam)[indexRobot].buffItem2 = item;
-        }
-    }
+		if (_itemState == ItemState.Update)
+		{
+			// Update damage value
+			_teamManager.GetRobots(_currentTeam)[indexRobot].damage = statDisplays[0].Stat.Value;
 
-    public void UpdateEquipmentId(EquippableItem item, string type)
-    {
-        if (type == EquipmentType.Top.ToString())
-        {
+			// Update armor value
+			_teamManager.GetRobots(_currentTeam)[indexRobot].armor = statDisplays[1].Stat.Value;
 
-            // Update helmet id
-            _teamManager.GetRobots(_currentTeam)[indexRobot].helmetId = item;
+		}
 
-        }
-        else if (type == EquipmentType.Body.ToString())
-        {
+	}
 
-            // Update armor id
-            _teamManager.GetRobots(_currentTeam)[indexRobot].armorId = item;
+	private void OnValidate()
+	{
+		Debug.Log("OnValidate");
+		_itemState = ItemState.Update;
+	}
 
-        }
-        else if (type == EquipmentType.Weapon.ToString())
-        {
+	// charStats = Armor, Attack
+	public void SetStats(params CharacterStat[] charStats)
+	{
+		stats = charStats;
 
-            // Update weapon id
-            _teamManager.GetRobots(_currentTeam)[indexRobot].weaponId = item;
-        }
+		// below stat display length
+		if (stats.Length > statDisplays.Length)
+		{
+			Debug.LogError("Not Enough Stat Display!");
+			return;
+		}
 
-    }
+		for (int i = 0; i < statDisplays.Length; i++)
+		{
+			statDisplays[i].gameObject.SetActive(i < stats.Length);
 
+			if (i < stats.Length)
+			{
+				statDisplays[i].Stat = stats[i];
+			}
+		}
+	}
 
+	public void UpdateStatValues()
+	{
+		for (int i = 0; i < stats.Length; i++)
+		{
+			statDisplays[i].UpdateStatValue();
+		}
+	}
 
-    public void UnequipId(string type)
-    {
-        if (type == EquipmentType.Top.ToString())
-        {
+	public void UpdateStatNames()
+	{
+		_itemState = ItemState.Update;
+		for (int i = 0; i < statNames.Length; i++)
+		{
+			statDisplays[i].Name = statNames[i];
+		}
+	}
+	public void UpdateUsableItemId(UsableItem item, string type)
+	{
+		if (type == EquipmentType.BuffItem1.ToString())
+		{
 
-            // Update helmet id
-            _teamManager.GetRobots(_currentTeam)[indexRobot].helmetId = null;
+			// Update Buff Item 1 id
+			_teamManager.GetRobots(_currentTeam)[indexRobot].buffItem1 = item;
+
+		}
+		else if (type == EquipmentType.BuffItem2.ToString())
+		{
 
-        }
-        else if (type == EquipmentType.Body.ToString())
-        {
+			// Update Buff Item 2 id
+			_teamManager.GetRobots(_currentTeam)[indexRobot].buffItem2 = item;
+		}
+	}
 
-            // Update armor id
-            _teamManager.GetRobots(_currentTeam)[indexRobot].armorId = null;
+	public void UpdateEquipmentId(EquippableItem item, string type)
+	{
+		if (type == EquipmentType.Top.ToString())
+		{
 
-        }
-        else if (type == EquipmentType.Weapon.ToString())
-        {
+			// Update helmet id
+			_teamManager.GetRobots(_currentTeam)[indexRobot].helmetId = item;
 
-            // Update weapon id
-            _teamManager.GetRobots(_currentTeam)[indexRobot].weaponId = null;
-        }
-        else if (type == EquipmentType.BuffItem1.ToString())
-        {
+		}
+		else if (type == EquipmentType.Body.ToString())
+		{
 
-            // Update Buff Item 1 id
-            _teamManager.GetRobots(_currentTeam)[indexRobot].buffItem1 = null;
+			// Update armor id
+			_teamManager.GetRobots(_currentTeam)[indexRobot].armorId = item;
 
-        }
-        else if (type == EquipmentType.BuffItem2.ToString())
-        {
+		}
+		else if (type == EquipmentType.Weapon.ToString())
+		{
 
-            // Update Buff Item 2 id
-            _teamManager.GetRobots(_currentTeam)[indexRobot].buffItem2 = null;
-        }
-    }
+			// Update weapon id
+			_teamManager.GetRobots(_currentTeam)[indexRobot].weaponId = item;
+		}
 
-    // Unity Event
-    public void ChangeTeam()
-    {
-        _currentTeam = _teamManager.teamB;
-        characterName.text = "Robot " + indexRobot.ToString() + " | Team " + _currentTeam.teamName;
+	}
 
-        // Reset index robot
-        indexRobot = 0;
 
-        // Make sure 2 team have been selected equipment
-        if (_currentTeam == _teamManager.teamB)
-        {
-            // _itemState = ItemState.Finish;
-            buttonBattle.gameObject.SetActive(true);
-        }
 
-    }
+	public void UnequipId(string type)
+	{
+		if (type == EquipmentType.Top.ToString())
+		{
 
-    public Team GetCurrentTeam()
-    {
-        return _currentTeam;
-    }
+			// Update helmet id
+			_teamManager.GetRobots(_currentTeam)[indexRobot].helmetId = null;
 
-    public void ChangeRobotIndex(int index)
-    {
-        indexRobot = index;
-        characterName.text = "Robot " + indexRobot.ToString() + " | Team " + _currentTeam;
-    }
+		}
+		else if (type == EquipmentType.Body.ToString())
+		{
 
-    public Dictionary<String, EquippableItem> GetDetailEquipment(int index)
-    {
-        Dictionary<String, EquippableItem> statRobot = new Dictionary<String, EquippableItem>();
+			// Update armor id
+			_teamManager.GetRobots(_currentTeam)[indexRobot].armorId = null;
 
-        statRobot.Add("helmetId", _teamManager.GetRobots(_currentTeam)[index].helmetId);
-        statRobot.Add("armorId", _teamManager.GetRobots(_currentTeam)[index].armorId);
-        statRobot.Add("weaponId", _teamManager.GetRobots(_currentTeam)[index].weaponId);
+		}
+		else if (type == EquipmentType.Weapon.ToString())
+		{
 
-        return statRobot;
-    }
+			// Update weapon id
+			_teamManager.GetRobots(_currentTeam)[indexRobot].weaponId = null;
+		}
+		else if (type == EquipmentType.BuffItem1.ToString())
+		{
 
-    public Dictionary<String, UsableItem> GetDetailItemEquip(int index)
-    {
-        Dictionary<String, UsableItem> statRobot = new Dictionary<String, UsableItem>();
+			// Update Buff Item 1 id
+			_teamManager.GetRobots(_currentTeam)[indexRobot].buffItem1 = null;
 
-        statRobot.Add("itemBuff1", _teamManager.GetRobots(_currentTeam)[index].buffItem1);
-        statRobot.Add("itemBuff2", _teamManager.GetRobots(_currentTeam)[index].buffItem2);
+		}
+		else if (type == EquipmentType.BuffItem2.ToString())
+		{
 
-        return statRobot;
-    }
+			// Update Buff Item 2 id
+			_teamManager.GetRobots(_currentTeam)[indexRobot].buffItem2 = null;
+		}
+	}
 
-    public List<Dictionary<String, EquippableItem>> GetDetailEquipmentTeam(Team team)
-    {
+	// Unity Event
+	public void ChangeTeam()
+	{
+		_currentTeam = _teamManager.teamB;
+		characterName.text = "Robot " + indexRobot.ToString() + " | Team " + _currentTeam.teamName;
 
-        List<Dictionary<String, EquippableItem>> listStatRobot = new List<Dictionary<String, EquippableItem>>();
+		// Reset index robot
+		indexRobot = 0;
 
-        for (int index = 0; index < 3; index++)
-        {
-            Dictionary<String, EquippableItem> statRobot = new Dictionary<String, EquippableItem>();
+		// Make sure 2 team have been selected equipment
+		if (_currentTeam == _teamManager.teamB)
+		{
+			// _itemState = ItemState.Finish;
+			buttonBattle.gameObject.SetActive(true);
+		}
 
-            statRobot.Add("helmetId", _teamManager.GetRobots(team)[index].helmetId);
-            statRobot.Add("armorId", _teamManager.GetRobots(team)[index].armorId);
-            statRobot.Add("weaponId", _teamManager.GetRobots(team)[index].weaponId);
+	}
 
-            listStatRobot.Add(statRobot);
-        }
+	public Team GetCurrentTeam()
+	{
+		return _currentTeam;
+	}
 
-        return listStatRobot;
-    }
+	public void ChangeRobotIndex(int index)
+	{
+		indexRobot = index;
+		characterName.text = "Robot " + indexRobot.ToString() + " | Team " + _currentTeam;
+	}
 
-    public List<Dictionary<String, UsableItem>> GetDetailItemTeam(Team team)
-    {
+	public Dictionary<String, EquippableItem> GetDetailEquipment(int index)
+	{
+		Dictionary<String, EquippableItem> statRobot = new Dictionary<String, EquippableItem>();
 
-        List<Dictionary<String, UsableItem>> listStatRobot = new List<Dictionary<String, UsableItem>>();
+		statRobot.Add("helmetId", _teamManager.GetRobots(_currentTeam)[index].helmetId);
+		statRobot.Add("armorId", _teamManager.GetRobots(_currentTeam)[index].armorId);
+		statRobot.Add("weaponId", _teamManager.GetRobots(_currentTeam)[index].weaponId);
 
-        for (int index = 0; index < 3; index++)
-        {
-            Dictionary<String, UsableItem> statRobot = new Dictionary<String, UsableItem>();
+		return statRobot;
+	}
 
-            statRobot.Add("itemBuff1", _teamManager.GetRobots(team)[index].buffItem1);
-            statRobot.Add("itemBuff2", _teamManager.GetRobots(team)[index].buffItem2);
+	public Dictionary<String, UsableItem> GetDetailItemEquip(int index)
+	{
+		Dictionary<String, UsableItem> statRobot = new Dictionary<String, UsableItem>();
 
-            listStatRobot.Add(statRobot);
-        }
+		statRobot.Add("itemBuff1", _teamManager.GetRobots(_currentTeam)[index].buffItem1);
+		statRobot.Add("itemBuff2", _teamManager.GetRobots(_currentTeam)[index].buffItem2);
 
-        return listStatRobot;
-    }
+		return statRobot;
+	}
+
+	public List<Dictionary<String, EquippableItem>> GetDetailEquipmentTeam(Team team)
+	{
+
+		List<Dictionary<String, EquippableItem>> listStatRobot = new List<Dictionary<String, EquippableItem>>();
+
+		if (_teamManager.totalRobot == 1)
+		{
+			for (int index = 0; index < 1; index++)
+			{
+				Dictionary<String, EquippableItem> statRobot = new Dictionary<String, EquippableItem>();
+
+				statRobot.Add("helmetId", _teamManager.GetRobots(team)[index].helmetId);
+				statRobot.Add("armorId", _teamManager.GetRobots(team)[index].armorId);
+				statRobot.Add("weaponId", _teamManager.GetRobots(team)[index].weaponId);
+
+				listStatRobot.Add(statRobot);
+			}
+		}
+		else
+			for (int index = 0; index < 3; index++)
+			{
+				Dictionary<String, EquippableItem> statRobot = new Dictionary<String, EquippableItem>();
+
+				statRobot.Add("helmetId", _teamManager.GetRobots(team)[index].helmetId);
+				statRobot.Add("armorId", _teamManager.GetRobots(team)[index].armorId);
+				statRobot.Add("weaponId", _teamManager.GetRobots(team)[index].weaponId);
+
+				listStatRobot.Add(statRobot);
+			}
+
+		return listStatRobot;
+	}
+
+	public List<Dictionary<String, UsableItem>> GetDetailItemTeam(Team team)
+	{
+
+		List<Dictionary<String, UsableItem>> listStatRobot = new List<Dictionary<String, UsableItem>>();
+
+		if (_teamManager.totalRobot == 1)
+		{
+			for (int index = 0; index < 1; index++)
+			{
+				Dictionary<String, UsableItem> statRobot = new Dictionary<String, UsableItem>();
+
+				statRobot.Add("itemBuff1", _teamManager.GetRobots(team)[index].buffItem1);
+				statRobot.Add("itemBuff2", _teamManager.GetRobots(team)[index].buffItem2);
+
+				listStatRobot.Add(statRobot);
+			}
+		}
+		else
+		{
+			for (int index = 0; index < 3; index++)
+			{
+				Dictionary<String, UsableItem> statRobot = new Dictionary<String, UsableItem>();
+
+				statRobot.Add("itemBuff1", _teamManager.GetRobots(team)[index].buffItem1);
+				statRobot.Add("itemBuff2", _teamManager.GetRobots(team)[index].buffItem2);
+
+				listStatRobot.Add(statRobot);
+			}
+		}
+
+		return listStatRobot;
+	}
 
 }
 
 public enum ItemState
 {
-    Initialize,
-    Update,
-    Finish,
+	Initialize,
+	Update,
+	Finish,
 }
